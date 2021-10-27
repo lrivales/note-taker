429198a018918e5d762ccaf89b1b3572fe158d9b
Lawrence Rivales <lrivales@gmail.com>
2021-10-27 15:48:30 -0400

    Add note api functionality

---
+++ server.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const notes = require('./db/db.json');
+
+const app = express();
+
+app.use(express.urlencoded({ extended:true }));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+function createNewNote(body, array) {
+    const note = body;
+    array.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(array, null, 2)
+    );
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    const note = createNewNote(req.body, notes);
+    res.json(notes);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+});
